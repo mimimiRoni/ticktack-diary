e77e33ce12e3eb69d52c29c9db32ebc060ee8bf3
mimimiRoni <nakanemiu@gmail.com>
2025-01-30 20:51:12 +0900

    [update] パスワードのバリデーション追加

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -5,3 +5,11 @@ export const emailScheme = yup
   .required("メールアドレスを入力してください")
   .email("メールアドレスの形式として認識できません")
   .required("メールアドレスは必須です");
+
+export const passwordSchema = yup
+  .string()
+  .min(8, "8文字以上必要です")
+  .matches(/[A-Z]/, "大文字を含めてください")
+  .matches(/[a-z]/, "小文字を含めてください")
+  .matches(/[0-9]/, "数字を含めてください")
+  .required("パスワードは必須です");
