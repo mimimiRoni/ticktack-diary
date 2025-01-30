ae56b785425e5cec52cb71ed92d5706f6ff0d811
homi <87790194+mimimiRoni@users.noreply.github.com>
2025-01-31 07:23:55 +0900

    [update] バリデーションの更新 (#8)

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,15 +1,14 @@
 import * as yup from "yup";
 
-export const emailScheme = yup
+export const emailSchema = yup
   .string()
   .required("メールアドレスを入力してください")
-  .email("メールアドレスの形式として認識できません")
-  .required("メールアドレスは必須です");
+  .email("メールアドレスの形式として認識できません");
 
 export const passwordSchema = yup
   .string()
   .min(8, "8文字以上必要です")
-  .matches(/[A-Z]/, "大文字を含めてください")
-  .matches(/[a-z]/, "小文字を含めてください")
-  .matches(/[0-9]/, "数字を含めてください")
-  .required("パスワードは必須です");
+  .matches(/[A-Z]/, "大文字が必要です")
+  .matches(/[a-z]/, "小文字が必要です")
+  .matches(/[0-9]/, "数字が必要です")
+  .required("パスワードを入力してください");
