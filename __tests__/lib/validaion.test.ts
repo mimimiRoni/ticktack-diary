bbee75a24c9c0ac800a2b0e33a5a9ceb6201f76b
mimimiRoni <nakanemiu@gmail.com>
2025-01-31 07:09:33 +0900

    [update] バリデーションの更新

--- __tests__/lib/validaion.test.ts
+++ __tests__/lib/validaion.test.ts
@@ -1,42 +1,51 @@
-import { emailScheme, passwordSchema } from "../../src/lib/validation";
+import { emailSchema, passwordSchema } from "../../src/lib/validation";
 
 describe("email", () => {
   test("should return true if the value is a valid email", async () => {
     const validEmail = "example@exampl.com";
-    const result = await emailScheme.isValid(validEmail);
+    const result = await emailSchema.isValid(validEmail);
     expect(result).toBe(true);
   });
 
   test("should return false if the value is a invalid email", async () => {
     const invalidEmail = "invalid-email";
-    const result = await emailScheme.isValid(invalidEmail);
-    expect(result).toBe(false);
+    expect(emailSchema.validate(invalidEmail)).rejects.toThrow(
+      "メールアドレスの形式として認識できません",
+    );
+  });
+
+  test("should return false if no value", async () => {
+    expect(emailSchema.validate("")).rejects.toThrow(
+      "メールアドレスを入力してください",
+    );
   });
 });
 
 describe("password", () => {
   test("should return false if the value is not contains A-Z", async () => {
     const invalid = "12345678abc";
-    const result = await passwordSchema.isValid(invalid);
-    expect(result).toBe(false);
+    expect(passwordSchema.validate(invalid)).rejects.toThrow(
+      "大文字が必要です",
+    );
   });
 
   test("should return false if the value is not contains a-z", async () => {
     const invalid = "12345678ABC";
-    const result = await passwordSchema.isValid(invalid);
-    expect(result).toBe(false);
+    expect(passwordSchema.validate(invalid)).rejects.toThrow(
+      "小文字が必要です",
+    );
   });
 
   test("should return false if the value is not contains 0-9", async () => {
     const invalid = "Abcdefgh";
-    const result = await passwordSchema.isValid(invalid);
-    expect(result).toBe(false);
+    expect(passwordSchema.validate(invalid)).rejects.toThrow("数字が必要です");
   });
 
   test("should return false if the value is less than 8 characters", async () => {
     const invalid = "1234";
-    const result = await passwordSchema.isValid(invalid);
-    expect(result).toBe(false);
+    expect(passwordSchema.validate(invalid)).rejects.toThrow(
+      "8文字以上必要です",
+    );
   });
 
   test("should return true if the value is valid password", async () => {
