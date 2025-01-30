f622b9253ee1bb2c92c926fa2a2874858adbc0b5
mimimiRoni <nakanemiu@gmail.com>
2025-01-30 20:52:09 +0900

    [update] メールアドレスのバリデーションに必須項目ですよも追加

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -3,4 +3,5 @@ import * as yup from "yup";
 export const emailScheme = yup
   .string()
   .required("メールアドレスを入力してください")
-  .email("メールアドレスの形式として認識できません");
+  .email("メールアドレスの形式として認識できません")
+  .required("メールアドレスは必須です");
