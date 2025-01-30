c78d62284ab6f79a73ac249238dd17291d17a5fb
mimimiRoni <nakanemiu@gmail.com>
2025-01-30 20:25:33 +0900

    [add] パスワード入力フィールド
    (cherry picked from commit 5852723ddae513f04f216bd5270d4a0813860666)

---
+++ __tests__/components/forms/PasswordField.test.tsx
@@ -0,0 +1,48 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import PasswordField from "../../../src/components/forms/PasswordField";
+import { useForm, FormProvider } from "react-hook-form";
+
+const Wrapper = ({ children }: { children: React.ReactNode }) => {
+  const methods = useForm();
+  return <FormProvider {...methods}>{children}</FormProvider>;
+};
+
+test("should render input field", () => {
+  render(
+    <Wrapper>
+      <PasswordField name="password" label="password"></PasswordField>
+    </Wrapper>,
+  );
+
+  const inputElement = screen.getByRole("textbox");
+
+  expect(inputElement).toBeVisible();
+  expect(inputElement).toHaveAttribute("type", "password");
+
+  const toggleDisplayPassword = screen.getByRole("checkbox");
+
+  // クリックすると、パスワードが表示されるようになる
+  fireEvent.click(toggleDisplayPassword);
+  expect(inputElement).toHaveAttribute("type", "text");
+
+  // もう一回クリックするとまたパスワード非表示になる
+  fireEvent.click(toggleDisplayPassword);
+  expect(inputElement).toHaveAttribute("type", "password");
+});
+
+test("should render toggle label", () => {
+  render(
+    <Wrapper>
+      <PasswordField name="password" label="password"></PasswordField>
+    </Wrapper>,
+  );
+
+  // 最初はパスワードは非表示
+  expect(screen.getByText("パスワードを表示する")).toBeVisible();
+
+  // トグルクリックで表示になる
+  const inputElement = screen.getByRole("checkbox");
+  fireEvent.click(inputElement);
+
+  expect(screen.getByText("パスワードを表示しています")).toBeVisible();
+});
