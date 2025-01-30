3ae45abc876b434233e9576f09d7235aa64215a0
homi <87790194+mimimiRoni@users.noreply.github.com>
2025-01-30 21:08:10 +0900

    [update] パスワードのバリデーション追加 (#6)

--- __tests__/lib/validaion.test.ts
+++ __tests__/lib/validaion.test.ts
@@ -1,4 +1,4 @@
-import { emailScheme } from "../../src/lib/validation";
+import { emailScheme, passwordSchema } from "../../src/lib/validation";
 
 describe("email", () => {
   test("should return true if the value is a valid email", async () => {
@@ -11,5 +11,37 @@ describe("email", () => {
     const invalidEmail = "invalid-email";
     const result = await emailScheme.isValid(invalidEmail);
     expect(result).toBe(false);
+  });
+});
+
+describe("password", () => {
+  test("should return false if the value is not contains A-Z", async () => {
+    const invalid = "12345678abc";
+    const result = await passwordSchema.isValid(invalid);
+    expect(result).toBe(false);
+  });
+
+  test("should return false if the value is not contains a-z", async () => {
+    const invalid = "12345678ABC";
+    const result = await passwordSchema.isValid(invalid);
+    expect(result).toBe(false);
+  });
+
+  test("should return false if the value is not contains 0-9", async () => {
+    const invalid = "Abcdefgh";
+    const result = await passwordSchema.isValid(invalid);
+    expect(result).toBe(false);
+  });
+
+  test("should return false if the value is less than 8 characters", async () => {
+    const invalid = "1234";
+    const result = await passwordSchema.isValid(invalid);
+    expect(result).toBe(false);
+  });
+
+  test("should return true if the value is valid password", async () => {
+    const valid = "Abcdefgh123";
+    const result = await passwordSchema.isValid(valid);
+    expect(result).toBe(true);
   });
 });
