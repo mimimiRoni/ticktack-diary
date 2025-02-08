4ba5a50c5c6a55cb5df005fa46f8ba692bab404c
homi <87790194+mimimiRoni@users.noreply.github.com>
2025-02-08 22:15:43 +0900

    [update] FirebaseConfigの値を一応Vercelの環境変数に変更 (#21)

--- src/configs/firebaseConfig.ts
+++ src/configs/firebaseConfig.ts
@@ -5,13 +5,13 @@ import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-p3TTOoz_gTT8wYGJsB1fGfms8P6oKC8",
-  authDomain: "ticktackdiary.firebaseapp.com",
-  projectId: "ticktackdiary",
-  storageBucket: "ticktackdiary.firebasestorage.app",
-  messagingSenderId: "838734934099",
-  appId: "1:838734934099:web:16de37ad1ec05a702e56a6",
-  measurementId: "G-JQ2CWPCTEJ",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
