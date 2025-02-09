67177353ce24716b66f60c3a91145e35ac1388ad
mimimiRoni <nakanemiu@gmail.com>
2025-02-09 11:47:55 +0900

    [update] TODOコメント移動

--- src/components/forms/SignUpForm.tsx
+++ src/components/forms/SignUpForm.tsx
@@ -22,6 +22,10 @@ const SignUpForm = () => {
   const onSubmit = async (data: { email: string; password: string }) => {
     setLoading(true);
     const userCredential = await signUpWithEmail(data.email, data.password);
+
+    // TODO: 既に登録済みのユーザーが登録しようとしたら、メールアドレスに確認メールを送る。
+    // メール認証がまだなら、認証メール
+    // メール認証が済んでいるなら、「新規登録しようとしたこと」「ログインURL」「パスワード忘れたらならこちら」のような内容のメールを送る
     await sendEmailVerification(userCredential.user);
     router.push("/verify-email");
     setLoading(false);
