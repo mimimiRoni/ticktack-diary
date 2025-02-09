07debd22f1d5cace0708949a39e0b2c3f79a7180
mimimiRoni <nakanemiu@gmail.com>
2025-02-09 18:29:38 +0900

    [add] メールアドレス＆パスワードの新規登録カスタムフック追加

---
+++ __tests__/hooks/auth/useRegisterEmailUser.test.ts
@@ -0,0 +1,62 @@
+import { useRegisterEmailUser } from "@/hooks/auth/useRegisterEmailUser";
+import { signUpWithEmail } from "@/lib/authentication";
+import { renderHook } from "@testing-library/react";
+import { act } from "react";
+
+jest.mock("@/lib/authentication", () => ({
+  signUpWithEmail: jest.fn(),
+}));
+
+jest.mock("firebase/auth", () => ({
+  sendEmailVerification: jest.fn(),
+}));
+
+describe("useRegisterEmailUser", () => {
+  const mockEmail = "test@test.com";
+  const mockPassword = "Password123";
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("should have init values", async () => {
+    const { result } = renderHook(() => useRegisterEmailUser());
+
+    expect(result.current.isLoading).toBe(false);
+  });
+
+  test("should return true with valid arguments", async () => {
+    (signUpWithEmail as jest.Mock).mockResolvedValue({ user: {} });
+    const { result } = renderHook(() => useRegisterEmailUser());
+
+    await act(async () => {
+      const success = await result.current.handleRegister(
+        mockEmail,
+        mockPassword,
+      );
+
+      expect(success).toBe(true);
+    });
+
+    expect(result.current.isLoading).toBe(false);
+  });
+
+  test("should return false with invalid arguments", async () => {
+    (signUpWithEmail as jest.Mock).mockImplementation(() => {
+      throw new Error();
+    });
+
+    const { result } = renderHook(() => useRegisterEmailUser());
+
+    await act(async () => {
+      const success = await result.current.handleRegister(
+        "invalid-email",
+        "invalid-pass",
+      );
+
+      expect(success).toBe(false);
+    });
+
+    expect(result.current.isLoading).toBe(false);
+  });
+});
