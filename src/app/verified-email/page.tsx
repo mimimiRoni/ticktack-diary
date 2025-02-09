630575643e855237aa14717fd8cccd8a241a3839
homi <87790194+mimimiRoni@users.noreply.github.com>
2025-02-10 07:34:41 +0900

    [update] メールアドレス認証完了ページを追加 (#25)
    * [update] メールアドレス認証完了ページを追加
    
    * [update] TODO更新
    
    * [update] window不要だったので削除
    
    * [update] Next.jsの書き方に変更
    
    * [update]ビルドエラー修正
    
    useSearchParams() should be wrapped in a suspense boundary at page "/verified-email". Read more: https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
    
    * [update] ビルドエラーを今度こそ解決

---
+++ src/app/verified-email/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { auth } from "@/configs/firebaseConfig";
+import { applyActionCode } from "firebase/auth";
+import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+
+/**
+ * メールアドレス認証を完了するためのページを表示するコンポーネント
+ * @returns メールアドレス認証を完了するためのページ JSX 要素
+ */
+export default function VerifiedEmail() {
+  return (
+    <div>
+      <h1>メールアドレス認証を完了するためのページ</h1>
+      <button>
+        <Link href="/">トップへ</Link>
+      </button>
+      <button>
+        <Link href="/timer">時間計測ページへ</Link>
+      </button>
+      <Suspense>
+        <Verified />
+      </Suspense>
+    </div>
+  );
+}
+
+/**
+ * メールアドレス認証を処理するコンポーネント
+ * @returns メールアドレス認証の結果による表示
+ */
+function Verified() {
+  const urlParams = useSearchParams();
+  const oobCode = urlParams.get("oobCode"); // URLパラメータからoobCodeを取得
+  const router = useRouter();
+
+  if (oobCode) {
+    applyActionCode(auth, oobCode)
+      .then(() => {
+        router.push("/login");
+      })
+      .catch();
+  }
+
+  return (
+    <>
+      {oobCode ? (
+        <p>メールの認証を完了しています……</p>
+      ) : (
+        <div>
+          <p>
+            メールの認証に失敗しました。一度ログインすることで、認証メールの再送信ができます。
+          </p>
+          <button>
+            <Link href="/login">ログインページへ</Link>
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
