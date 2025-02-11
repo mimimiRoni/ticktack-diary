304cdcafcf638a9ecc593fc4950f65cdf3d988e0
mimimiRoni <nakanemiu@gmail.com>
2025-02-11 12:16:11 +0900

    [update] Firestoreに無駄に仮登録データを保存するのをやめる
    メール認証はするが、不必要に仮データをとっておいて仮のままのユーザーを削除とかまでする必要がないので変更

--- src/lib/authentication.ts
+++ src/lib/authentication.ts
@@ -1,10 +1,8 @@
-import { auth, db } from "@/configs/firebaseConfig";
-import { DraftUser } from "@/types/DraftUser.type";
+import { auth } from "@/configs/firebaseConfig";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
-import { doc, setDoc, Timestamp } from "firebase/firestore";
 
 export const signUpWithEmail = async (email: string, password: string) => {
   try {
@@ -13,20 +11,6 @@ export const signUpWithEmail = async (email: string, password: string) => {
       email,
       password,
     );
-
-    // Firestoreに仮ユーザーとして登録し、TTLで24時間以内にメールアドレス認証が完了しなければFirestroreとAuthから削除する
-    const draftUser: DraftUser = {
-      email: email,
-      verified: false,
-      createdAt: new Date(Date.now()),
-      expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
-    };
-
-    await setDoc(doc(db, "users", userCredential.user.uid), {
-      ...draftUser,
-      createdAt: Timestamp.fromDate(draftUser.createdAt),
-      expiresAt: Timestamp.fromDate(draftUser.expiresAt!),
-    });
 
     return userCredential;
   } catch (error) {
