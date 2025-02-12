a1b3a058e6f1cf614165e37b11f2cfada668f2b6
mimimiRoni <nakanemiu@gmail.com>
2025-02-12 19:42:04 +0900

    [update] メールアドレス認証ページもざっくり動作確認できるように更新

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,10 +1,34 @@
+"use client";
+
+import { auth } from "@/configs/firebaseConfig";
+import { useLoggedInUser } from "@/hooks/auth/useLoggedInUser";
+import { sendEmailVerification, signOut } from "firebase/auth";
 import Link from "next/link";
+import { useState } from "react";
 
 /**
  * メールアドレス認証ページを表示するコンポーネント
  * @returns メールアドレス認証ページの JSX 要素
  */
 export default function VerifyEmail() {
+  // TODO: 動作確認用のログアウト処理
+  const logOutHandler = async () => {
+    await signOut(auth);
+  };
+
+  // TODO: ざっくり動作確認程度なので、後ほど見直す
+  const { loginUser } = useLoggedInUser();
+  const [isSendedVerifyEmail, setSendedVerifyEmail] = useState(false);
+
+  const sendedVerifyEmailHandler = async () => {
+    try {
+      if (loginUser) {
+        await sendEmailVerification(loginUser);
+        setSendedVerifyEmail(true);
+      }
+    } catch {}
+  };
+
   return (
     <div>
       <h1>メールアドレス認証ページ</h1>
@@ -14,12 +38,15 @@ export default function VerifyEmail() {
       <button>
         <Link href="/timer">時間計測ページへ</Link>
       </button>
-      {/* TODO: メール認証が完了したら、一度ログアウトさせて再度ログインさせるようにする */}
-      <p>ご登録いただいたメールアドレスに、認証メールを送信しました！</p>
-      <p>認証メールのリンクをクリックして、アクセスしてください。</p>
+      <button onClick={() => logOutHandler()}>ログアウト</button>
       <p>
-        認証メールのリンクをクリックすることで、メールアドレスの認証が完了します。
+        {isSendedVerifyEmail === true
+          ? "ご登録いただいたメールアドレスに、認証メールを送信しました！届かない場合は下の送信ボタンから再送信してください"
+          : "ご登録いただいたメールアドレスに、認証メールを送信します。下の送信ボタンを押してください。"}
       </p>
+      <p>認証メールのリンクにアクセスしてください。</p>
+      <p>メールアドレスの認証が完了します。</p>
+      <button onClick={() => sendedVerifyEmailHandler()}>認証を送信</button>
     </div>
   );
 }
