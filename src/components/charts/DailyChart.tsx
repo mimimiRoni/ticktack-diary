c4ae12de31e7b4754539e2bb5781944380bc737d
mimimiRoni <nakanemiu@gmail.com>
2025-02-17 20:24:56 +0900

    [update] 棒グラフでmsから変換した文字をTooltipに表示するように変更

--- src/components/charts/DailyChart.tsx
+++ src/components/charts/DailyChart.tsx
@@ -19,13 +19,24 @@ interface DailyChartProps {
 }
 
 export const DailyChart: React.FC<DailyChartProps> = ({ data }) => {
+  const formatter = (ms: number) => {
+    const seconds = Math.floor(ms / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    return hours >= 1
+      ? `${hours} 時間 ${minutes - hours * 60} 分 ${seconds - minutes * 60} 秒`
+      : minutes >= 1
+        ? `${minutes} 分 ${seconds - minutes * 60} 秒`
+        : `${seconds} 秒`;
+  };
+
   return (
     <ResponsiveContainer width="100%" height={300}>
       <BarChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="day" />
         <YAxis />
-        <Tooltip />
+        <Tooltip formatter={formatter} />
         <Bar type="monotone" dataKey="totalTime" fill="#82ca9d" />
       </BarChart>
     </ResponsiveContainer>
