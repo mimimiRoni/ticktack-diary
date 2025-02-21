58b94e4fa376f2a21de5742ee5af49b784aa83a1
mimimiRoni <nakanemiu@gmail.com>
2025-02-21 18:38:52 +0900

    [update] SCSSに置き換え

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    prependData: `@use "src/styles/globals" as *;`,
+  },
 };
 
 export default nextConfig;
