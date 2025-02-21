2c699e837c6a7d496d6935ed8b9bf0539c5c9043
homi <87790194+mimimiRoni@users.noreply.github.com>
2025-02-21 18:41:03 +0900

    [update] SCSSに置き換え (#51)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Inter, Noto_Sans_JP } from "next/font/google";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const notoSansJP = Noto_Sans_JP({
+  variable: "--font-noto-sans-jp",
   subsets: ["latin"],
 });
 
@@ -31,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${inter.variable} ${notoSansJP.variable} root`}>
         {children}
         <Footer />
       </body>
