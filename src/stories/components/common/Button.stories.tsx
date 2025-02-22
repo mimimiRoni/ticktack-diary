6226680e570609da07cde3a6ee9991f2e2b8f5e2
mimimiRoni <nakanemiu@gmail.com>
2025-02-22 09:11:12 +0900

    [add] Button追加。不要になったサンプルを削除

---
+++ src/stories/components/common/Button.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Button from "@/components/common/Button/Button";
+
+const meta = {
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Playground: Story = {
+  args: {
+    variant: "fill",
+    children: "Button",
+    disabled: false,
+  },
+};
+
+export const AllButtons: Story = {
+  render: () => (
+    <div>
+      <h2>塗りボタン</h2>
+      <div>
+        <Button variant="fill">Fill</Button>
+        <Button variant="fill" disabled={true}>
+          Fill disabled
+        </Button>
+      </div>
+      <h2>アウトラインボタン</h2>
+      <div>
+        <Button variant="outline">Outline</Button>
+        <Button variant="outline" disabled={true}>
+          Outline disabled
+        </Button>
+      </div>
+      <h2>テキストボタン</h2>
+      <div>
+        <Button variant="text">Text</Button>
+        <Button variant="text" disabled={true}>
+          Text disabled
+        </Button>
+      </div>
+    </div>
+  ),
+};
+
+export const LinkButton: Story = {
+  render: () => (
+    <div>
+      <h2>リンクボタン</h2>
+      <div>
+        <Button>
+          <a>Link</a>
+        </Button>
+        <Button disabled={true}>
+          <a>Link</a>
+        </Button>
+      </div>
+    </div>
+  ),
+};
