c2862c18ab13307fe5938f6702695cf3d0f9b249
mimimiRoni <nakanemiu@gmail.com>
2025-02-24 11:43:05 +0900

    Revert "[update] ワークフローで正しく失敗することが確認できたので、Formatしたものを上げる"
    This reverts commit e69cbb90c02a91d7d00b759b552c38c177cd251e.

--- api/index.go
+++ api/index.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 )
-
+ 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
