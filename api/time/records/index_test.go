be9238714777397c05075e69b38bd8ba5a21af81
mimimiRoni <nakanemiu@gmail.com>
2025-03-02 21:05:19 +0900

    [add] 時間記録の追加エンドポイント追加

---
+++ api/time/records/index_test.go
@@ -0,0 +1,176 @@
+package records_test
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"ticktack-diary/api/_util/auth"
+	"ticktack-diary/api/_util/db"
+	"ticktack-diary/api/time/records"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/stretchr/testify/assert"
+)
+
+type testCase struct {
+	name            string
+	mockVerifyToken func(r *http.Request) (string, error)
+	mockGetDB       func() (*pgxpool.Pool, error)
+	requestBody     interface{}
+	expectedCode    int
+	expectedBody    map[string]interface{}
+}
+
+const apiUrl = "/api/time/records"
+
+var validVerifyToken = func(r *http.Request) (string, error) {
+	return "mock-user", nil
+}
+
+var validMockGetDB = func() (*pgxpool.Pool, error) {
+	pool, _ := pgxpool.New(context.Background(), "")
+	return pool, nil
+}
+
+var validRequestBody = map[string]string{
+	"category-id": "00000000-0000-0000-0000-000000000000",
+	"started-at":  "2025-03-02T04:22:31.958Z",
+	"duration-ms": "3600000",
+}
+
+func TestNotAllowMethod(t *testing.T) {
+	originalVerifyToken := auth.VerifyToken
+	defer func() {
+		auth.VerifyToken = originalVerifyToken
+	}()
+
+	auth.VerifyToken = func(r *http.Request) (string, error) {
+		return "mock-user", nil
+	}
+
+	var reqBody []byte
+	var err error
+
+	reqBody, err = json.Marshal(validRequestBody)
+
+	if err != nil {
+		t.Fatalf("failed JSON encode: %v", err)
+	}
+
+	request := httptest.NewRequest(http.MethodConnect, apiUrl, bytes.NewBuffer(reqBody))
+	request.Header.Set("Content-Type", "application/json")
+	response := httptest.NewRecorder()
+
+	records.Handler(response, request)
+
+	assert.Equal(t, http.StatusMethodNotAllowed, response.Code)
+}
+
+func TestHandlerPost(t *testing.T) {
+	originalVerifyToken := auth.VerifyToken
+	originalGetDB := db.GetDB
+	defer func() {
+		auth.VerifyToken = originalVerifyToken
+		db.GetDB = originalGetDB
+	}()
+
+	assert := assert.New(t)
+
+	tests := []testCase{
+		{
+			name:            "should return 201 Created",
+			mockVerifyToken: validVerifyToken,
+			mockGetDB:       validMockGetDB,
+			requestBody:     validRequestBody,
+			expectedCode:    http.StatusCreated,
+			expectedBody:    map[string]interface{}{"uid": "mock-user"},
+		},
+		{
+			name: "should return failed when failed to verify token",
+			mockVerifyToken: func(r *http.Request) (string, error) {
+				return "", fmt.Errorf("invalid auth token")
+			},
+			mockGetDB:    validMockGetDB,
+			requestBody:  validRequestBody,
+			expectedCode: http.StatusUnauthorized,
+			expectedBody: map[string]interface{}{"error": "invalid auth token"},
+		},
+		{
+			name:            "should return failed when failed to decode request",
+			mockVerifyToken: validVerifyToken,
+			mockGetDB:       validMockGetDB,
+			requestBody:     nil,
+			expectedCode:    http.StatusBadRequest,
+			expectedBody:    map[string]interface{}{"error": "failed to request JSON decode. EOF"},
+		},
+		{
+			name:            "should return 400 Bad Request when invalid category id",
+			mockVerifyToken: validVerifyToken,
+			mockGetDB:       validMockGetDB,
+			requestBody: map[string]string{
+				"category-id": "invalid",
+				"started-at":  "2025-03-02T04:22:31.958Z",
+				"duration-ms": "3600000",
+			},
+			expectedCode: http.StatusBadRequest,
+			expectedBody: map[string]interface{}{"error": "failed to request validation. Key: 'TimeRecordPostRequest.CategoryID' Error:Field validation for 'CategoryID' failed on the 'uuid' tag"},
+		},
+		{
+			name:            "should return 400 Bad Request when invalid duration",
+			mockVerifyToken: validVerifyToken,
+			mockGetDB:       validMockGetDB,
+			requestBody: map[string]string{
+				"category-id": "00000000-0000-0000-0000-000000000000",
+				"started-at":  "2025-03-02T04:22:31.958Z",
+				"duration-ms": "0",
+			},
+			expectedCode: http.StatusBadRequest,
+			expectedBody: map[string]interface{}{"error": "failed to request validation. Key: 'TimeRecordPostRequest.DurationMs' Error:Field validation for 'DurationMs' failed on the 'min' tag"},
+		},
+		{
+			name:            "should return 500 when failed connect db",
+			mockVerifyToken: validVerifyToken,
+			mockGetDB:       func() (*pgxpool.Pool, error) { return nil, fmt.Errorf("failed to connect db") },
+			requestBody:     validRequestBody,
+			expectedCode:    http.StatusInternalServerError,
+			expectedBody:    map[string]interface{}{"error": "failed to connect db"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			auth.VerifyToken = tt.mockVerifyToken
+			db.GetDB = tt.mockGetDB
+			var reqBody []byte
+			var err error
+
+			if tt.requestBody != nil {
+				reqBody, err = json.Marshal(tt.requestBody)
+				if err != nil {
+					t.Fatalf("failed JSON encode: %v", err)
+				}
+			}
+
+			request := httptest.NewRequest(http.MethodPost, apiUrl, bytes.NewBuffer(reqBody))
+			request.Header.Set("Content-Type", "application/json")
+			response := httptest.NewRecorder()
+
+			records.Handler(response, request)
+
+			assert.Equal(tt.expectedCode, response.Code)
+			assert.Equal("application/json", response.Header().Get("Content-Type"))
+
+			var actualBody map[string]interface{}
+			err = json.Unmarshal(response.Body.Bytes(), &actualBody)
+			if err != nil {
+				t.Fatalf("failed response JSON decode: %v", err)
+			}
+
+			assert.Equal(tt.expectedBody, actualBody, tt.expectedBody)
+		})
+	}
+}
